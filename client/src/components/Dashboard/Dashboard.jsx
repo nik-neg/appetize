f02baaf59ae33f06d145099dcf403070e2e69920
Alexander Genkin <alexander_genkin@gmx.net>
2021-04-24 15:04:47 +0200

    add: card text aligment, refactor for dashboard

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,16 +1,25 @@
-import LocalDishesParameter from '../LocalDishesParameter/LocalDischesParameter';
+// import LocalDishesParameter from '../LocalDishesParameter/LocalDischesParameter';
 import Card from '../Card/Card'
-// import { useState } from 'react';
+// import { useState, useEffect } from 'react';
 
 
 export default function Dashboard ({id}) {
 
   // const [mouthWateringDishes, setMouthWateringDishes] = useState([]);
 
+  // useEffect(() => {
+  // setMouthWateringDishes(mouthWateringDishes)
+  // }, [mouthWateringDishes]);
+
   return (
     <div>
       Hello Dashboard.
-      <LocalDishesParameter/>
+      {/* <LocalDishesParameter/>
+      {mouthWateringDishes.length > 0 ?
+        mouthWateringDishes.map((dish) => {
+          return <Card id={id}/>
+        })
+      : ''} */}
       <Card id={id}/>
     </div>
   );
