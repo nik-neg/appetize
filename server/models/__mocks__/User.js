293334dac59ab34000a5743ff55a00552383a82a
Alexander Genkin <alexander_genkin@gmx.net>
2021-10-01 16:47:04 +0200

    test: add unit test for publish controller - up/down vote dish

--- server/models/__mocks__/User.js
+++ server/models/__mocks__/User.js
@@ -7,6 +7,7 @@ const User = {
   hashedPassword: Array.from('password').reverse().join(''),
   findOne: async () => {},
   create: async () => {},
+  findOneAndUpdate: async () => {},
 };
 
 module.exports = User;
