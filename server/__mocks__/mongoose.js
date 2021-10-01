33bd5d3719a7b8df51535af16d57cdab15965ac2
Alexander Genkin <alexander_genkin@gmx.net>
2021-10-01 21:18:02 +0200

    test: add unit test for db helper - remove image data

--- server/__mocks__/mongoose.js
+++ server/__mocks__/mongoose.js
@@ -1,8 +1,15 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
+const deleteOne = { deletedCount: 1 };
+const deleteMany = { deletedCount: 3 };
 module.exports = {
   connection: {
     db: {
-      collection: () => {},
+      collection: (bucketPath) => ({
+        find: () => ({ toArray: () => ({ map: () => [123, 456, 789] }) }),
+        deleteOne: async () => deleteOne,
+        deleteMany: async () => deleteMany,
+      }),
     },
   },
 };
