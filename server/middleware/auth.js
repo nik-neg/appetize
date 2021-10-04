ac4ca44b4f08bcdef0d9c5cc0695be4ff67afcad
Alexander Genkin <alexander_genkin@gmx.net>
2021-10-04 15:06:54 +0200

    test: add unit tests for auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,17 +6,22 @@ const SECRET_KEY = process.env.SECRET_KEY || 'loading'; // TODO: check key
 
 const authMiddleware = async (req, res, next) => {
   const authHeaders = req.headers.authorization;
-  if (!authHeaders) return res.sendStatus(403);
+  if (!authHeaders) return res.status(403).send();
   const token = authHeaders.split(' ')[1];
   try {
     const { _id } = jwt.verify(token, SECRET_KEY);
-    let user = await User.findOne({ _id });
+    let user;
+    try {
+      user = await User.findOne({ _id });
+    } catch (err) {
+      return res.status(500).send({ error: '500', message: 'Could not find user - Internal server error' });
+    }
+    if (!user) return res.status(401).send();
     user = _.omit(user, ['password']);
-    if (!user) return res.sendStatus(401);
     req.user = user;
     next();
   } catch (error) {
-    res.sendStatus(401);
+    return res.status(401).send();
   }
 };
 
