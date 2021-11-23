e4d77c12f1c12682d711605e8e3ade98a2bc94cb
Alexander Genkin <alexander_genkin@gmx.net>
2021-11-23 12:03:16 +0100

    fix: indent issue, import order

--- server/api/user/login/user.js
+++ server/api/user/login/user.js
@@ -1,38 +1,34 @@
-'use strict';
-
-const db = require('../../models/db');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const db = require('../../models/db');
 
 const SECRET_KEY = process.env.SECRET_KEY || 'loading';
-
-const saltRounds = 10;
 
 const User = require('../../models/User');
 
 const createConn = async () => {
-		await db.connect();
+  await db.connect();
 };
 
 module.exports.login = async (event) => {
-	const response = {};
-	if (!db.mongoose.connection.readyState) {
-		// Cold start or connection timed out. Create new connection.
-		try {
-				await createConn();
-		} catch (e) {
-			response.error = e.message;
-			return response;
-		}
-	}
+  const response = {};
+  if (!db.mongoose.connection.readyState) {
+    // Cold start or connection timed out. Create new connection.
+    try {
+      await createConn();
+    } catch (e) {
+      response.error = e.message;
+      return response;
+    }
+  }
   const { email, password } = JSON.parse(event.body);
   try {
     const invalidInput = !email || !password;
     if (invalidInput) throw new Error();
   } catch (error) {
-		response.statusCode = 400;
-		response.body = { error: '400', message: 'Please provide login credentials' };
-		return response;
+    response.statusCode = 400;
+    response.body = { error: '400', message: 'Please provide login credentials' };
+    return response;
   }
 
   let user;
@@ -41,22 +37,22 @@ module.exports.login = async (event) => {
       email,
     });
   } catch (err) {
-		response.statusCode = 500;
-		response.body = { error: '500', message: 'Could not find user - Internal server error'};
-		return response;
+    response.statusCode = 500;
+    response.body = { error: '500', message: 'Could not find user - Internal server error'};
+    return response;
   }
   try {
     // const checkedPassword = await bcrypt.compare(password, user.password);
     // if (!checkedPassword) throw new Error();
     const { _id } = user;
     const accessToken = jwt.sign({ _id }, SECRET_KEY);
-		response.statusCode = 200;
-		response.body = JSON.stringify({ user: _.omit(user._doc, ['password']), accessToken });
-		return response;
+    response.statusCode = 200;
+    response.body = JSON.stringify({ user: _.omit(user._doc, ['password']), accessToken });
+    return response;
   } catch (error) {
-		response.statusCode = 401;
-		response.body = { error: '401', message: 'email or password is incorrect' };
-		return response;
+    response.statusCode = 401;
+    response.body = { error: '401', message: 'email or password is incorrect' };
+    return response;
   }
 
 };
--- server/api/user/register/user.js
+++ server/api/user/register/user.js
@@ -1,9 +1,9 @@
-'use strict';
+/* eslint-disable no-underscore-dangle */
 
-const db = require('../../models/db');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const db = require('../../models/db');
 
 const SECRET_KEY = process.env.SECRET_KEY || 'loading';
 
@@ -12,79 +12,62 @@ const saltRounds = 10;
 const User = require('../../models/User');
 
 const createConn = async () => {
-		await db.connect();
+  await db.connect();
 };
 
 module.exports.register = async (event) => {
-	// console.log('Event: ', event);
-  // let responseMessage = 'Hello, World!';
+  const response = {};
+  if (!db.mongoose.connection.readyState) {
+    // Cold start or connection timed out. Create new connection.
+    try {
+      await createConn();
+    } catch (e) {
+      response.error = e.message;
+      return response;
+    }
+  }
 
-  //  if (event.queryStringParameters && event.queryStringParameters['Name']) {
-  //     responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
-  //   }
+  const {
+    firstName, lastName, email, password,
+  } = JSON.parse(event.body);
 
-  // return {
-  //   statusCode: 200,
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify({
-  //     message: responseMessage,
-  //   }),
-  // }
+  let user = await User.findOne({
+    email,
+  });
 
-	const response = {};
-	if (!db.mongoose.connection.readyState) {
-		// Cold start or connection timed out. Create new connection.
-		try {
-				await createConn();
-		} catch (e) {
-			response.error = e.message;
-			return response;
-		}
-	}
+  if (user) {
+    response.statusCode = 409;
+    response.body = JSON.stringify({ error: '409', message: 'User already exists' });
+    return response;
+  }
+  try {
+    const invalidInput = !firstName || !lastName || !email || !password;
+    if (invalidInput) throw new Error();
+  } catch (error) {
+    response.statusCode = 400;
+    response.body = JSON.stringify({ error: '400', message: 'Please provide credentials' });
+    return response;
+  }
 
-	const {
-		firstName, lastName, email, password,
-	} = JSON.parse(event.body);
-
-	let user = await User.findOne({
-		email,
-	});
-
-	if (user) {
-		response.statusCode = 409;
-		response.body = JSON.stringify({ error: '409', message: 'User already exists' });
-		return response;
-	}
-	try {
-		const invalidInput = !firstName || !lastName || !email || !password;
-		if (invalidInput) throw new Error();
-	} catch (error) {
-		response.statusCode = 400;
-		response.body = JSON.stringify({ error: '400', message: 'Please provide credentials' });
-		return response;
-	}
-
-	try {
-		const hash = password;
-		const newUser = await User.create({
-			firstName,
-			lastName,
-			email,
-			password: hash,
-			created: new Date(),
-		});
-		user = await newUser.save();
-		db.mongoose.connection.close(); // TODO: useful?
-	} catch (err) {
-			response.statusCode = 500;
-			response.body = JSON.stringify({ error: '500', message: 'Could not create user - Internal server error' });
-			return response;
-	}
-	const { _id } = user;
-	const accessToken = jwt.sign({ _id }, SECRET_KEY);
-	response.statusCode = 201;
-	response.body = JSON.stringify({ user: _.omit(user._doc, ['password']), accessToken });
-	return response;
+  try {
+    const hash = password;
+    const newUser = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hash,
+      created: new Date(),
+    });
+    user = await newUser.save();
+    db.mongoose.connection.close(); // TODO: useful?
+  } catch (err) {
+    response.statusCode = 500;
+    response.body = JSON.stringify({ error: '500', message: 'Could not create user - Internal server error' });
+    return response;
+  }
+  const { _id } = user;
+  const accessToken = jwt.sign({ _id }, SECRET_KEY);
+  response.statusCode = 201;
+  response.body = JSON.stringify({ user: _.omit(user._doc, ['password']), accessToken });
+  return response;
 };
