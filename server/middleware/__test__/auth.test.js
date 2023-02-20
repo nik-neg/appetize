e196954426f8f2771360859476cd63e506e929b6
Alexander Genkin <alexander_genkin@gmx.net>
2023-02-20 15:05:31 +0200

    try: fix tests

--- server/middleware/__test__/auth.test.js
+++ server/middleware/__test__/auth.test.js
@@ -41,52 +41,55 @@ describe("authMiddleware method", () => {
     expect(res.status).toHaveBeenCalledTimes(1);
     expect(res.send).toHaveBeenCalledTimes(1);
   });
-  // test("authMiddleware returns 401, because of malformed jwt token", async () => {
-  //   const { req, res } = setup();
-  //   req.headers = { authorization: "first token" };
-  //   jwt.verify.mockRejectedValue("some token");
-  //   await authMiddleware(req, res, () => {});
-  //   expect(res.status).toHaveBeenCalledWith(401);
-  //   expect(res.status).toHaveBeenCalledTimes(1);
-  //   expect(res.send).toHaveBeenCalledTimes(1);
-  // });
-  // test("authMiddleware returns 401, because user could not be found", async () => {
-  //   const { req, res } = setup();
-  //   req.headers = { authorization: "first token" };
-  //   jwt.verify.mockResolvedValue("some token");
-  //   await User.findOne.mockResolvedValue(null);
-  //   await authMiddleware(req, res, () => {});
-  //   expect(res.status).toHaveBeenCalledWith(401);
-  //   expect(res.status).toHaveBeenCalledTimes(1);
-  //   expect(res.send).toHaveBeenCalledTimes(1);
-  // });
-  // test('authMiddleware returns 500, because of internal server error', async () => {
-  //   const { req, res } = setup();
-  //   req.headers = { authorization: 'first token' };
-  //   jwt.verify.mockResolvedValue('some token');
-  //   const mockErr = new Error('ERROR');
-  //   await User.findOne.mockRejectedValue(mockErr);
-  //   await authMiddleware(req, res, () => {});
-  //   expect(res.status).toHaveBeenCalledWith(500);
-  //   expect(res.status).toHaveBeenCalledTimes(1);
-  //   expect(res.send).toHaveBeenCalledTimes(1);
-  // });
-  // test('authMiddleware set the user attribute in the request and calls the next function', async () => {
-  //   const { req, res } = setup();
-  //   req.headers = { authorization: 'first token' };
-  //   jwt.verify.mockResolvedValue('some token');
-  //   const {
-  //     firstName, lastName, email, password,
-  //   } = User;
-  //   const mockUser = {
-  //     firstName, lastName, email, password,
-  //   };
-  //   let user = await User.findOne.mockResolvedValue(mockUser);
-  //   user = await user();
-  //   user = _.omit(user, ['password']);
-  //   const next = jest.fn();
-  //   await authMiddleware(req, res, next);
-  //   expect(req.user).toEqual(user);
-  //   expect(next).toHaveBeenCalledTimes(1);
-  // });
+  test("authMiddleware returns 401, because of malformed jwt token", async () => {
+    const { req, res } = setup();
+    req.headers = { authorization: "first token" };
+
+    jwt.verify.mockRejectedValue("some token");
+    await authMiddleware(req, res, () => {});
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.send).toHaveBeenCalledTimes(1);
+  });
+  test("authMiddleware returns 401, because user could not be found", async () => {
+    const { req, res } = setup();
+    req.headers = { authorization: "first token" };
+    jwt.verify.mockResolvedValue("some token");
+    await User.findOne.mockResolvedValue(null);
+    await authMiddleware(req, res, () => {});
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.send).toHaveBeenCalledTimes(1);
+  });
+  test("authMiddleware returns 500, because of internal server error", async () => {
+    const { req, res } = setup();
+    req.headers = { authorization: "first token" };
+    jwt.verify.mockResolvedValue("some token");
+    const mockErr = new Error("ERROR");
+    await User.findOne.mockRejectedValue(mockErr);
+    await authMiddleware(req, res, () => {});
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.send).toHaveBeenCalledTimes(1);
+  });
+  test("authMiddleware set the user attribute in the request and calls the next function", async () => {
+    const { req, res } = setup();
+    req.headers = { authorization: "first token" };
+    jwt.verify.mockResolvedValue("some token");
+    const { firstName, lastName, email, password } = User;
+    const mockUser = {
+      firstName,
+      lastName,
+      email,
+      password,
+    };
+    let user = await User.findOne.mockResolvedValue(mockUser);
+    user = await user();
+    user = _.omit(user, ["password"]);
+    const next = jest.fn();
+    await authMiddleware(req, res, next);
+    expect(req.user).toEqual(user);
+    expect(next).toHaveBeenCalledTimes(1);
+  });
 });
